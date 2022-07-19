91b454d3375a3875cc75c4f1359ef5a9c798585e
Wongi <hpurs95@gmail.com>
2022-07-19 18:06:13 +0900

    Initial Commit

---
+++ funcAssignment3/funcAssignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  funcAssignment3App.swift
+//  funcAssignment3
+//
+//  Created by MacBook Air on 2022/07/19.
+//
+
+import SwiftUI
+
+@main
+struct funcAssignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
